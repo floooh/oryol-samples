fac8415d1e031ffdccaf010ad0fa164f733cd24b
Andre Weissflog <floooh@gmail.com>
2016-05-05 12:21:57 +0200

    Physics demo: simplify and restructure a bit

--- src/BulletPhysicsBasic/BulletPhysicsBasic.cc
+++ src/BulletPhysicsBasic/BulletPhysicsBasic.cc
@@ -21,9 +21,16 @@ public:
     AppState::Code OnInit();
     AppState::Code OnCleanup();
 
+    void setupGraphics();
+    void discardGraphics();
+    void drawGround();
+    void drawRigidBodies();
+
+    void setupPhysics();
+    void discardPhysics();
+    Duration updatePhysics(Duration frameTime);
+
     Id ground;
-    Id ballTexture;
-    Id groundTexture;
     DrawState drawState;
 
     glm::mat4 proj;
@@ -31,7 +38,7 @@ public:
     Shader::Params vsParams;
 
     int frameIndex = 0;
-    static const int MaxNumBodies = 1024;
+    static const int MaxNumBodies = 128;
     int numBodies = 0;
     StaticArray<Id, MaxNumBodies> bodies;
 
@@ -39,21 +46,55 @@ public:
 };
 OryolMain(BulletPhysicsBasicApp);
 
-// a simple ball texture
-uint32_t BallImageData[] = {
-    0xFF0000FF, 0xFF00FF00, 0xFFFF0000, 0xFF00FFFF, // red, green, blue, yello
-};
-
-// a checkboard ground texture
-static const int GroundTexSize = 16;
-uint32_t GroundImageData[GroundTexSize][GroundTexSize];
-
 //------------------------------------------------------------------------------
 AppState::Code
 BulletPhysicsBasicApp::OnInit() {
+    this->setupGraphics();
+    this->setupPhysics();
+    Dbg::Setup();
+    this->lapTimePoint = Clock::Now();
+    return App::OnInit();
+}
+
+//------------------------------------------------------------------------------
+AppState::Code
+BulletPhysicsBasicApp::OnRunning() {
+
+    this->frameIndex++;
+    Duration frameTime = Clock::LapTime(this->lapTimePoint);
+    Duration physicsTime = this->updatePhysics(frameTime);
+
+    Gfx::ApplyDefaultRenderTarget(ClearState::ClearAll(glm::vec4(0.2f, 0.4f, 0.8f, 1.0f), 1.0f, 0));
+    this->drawGround();
+    this->drawRigidBodies();
+
+    Dbg::PrintF("\n\r"
+                "  Frame time:   %.4f ms\n\r"
+                "  Physics time: %.4f ms\n\r"
+                "  Num Bodies:   %d\n\r",
+                frameTime.AsMilliSeconds(),
+                physicsTime.AsMilliSeconds(),
+                this->numBodies);
+    Dbg::DrawTextBuffer();
+    Gfx::CommitFrame();
+    return Gfx::QuitRequested() ? AppState::Cleanup : AppState::Running;
+}
+
+//------------------------------------------------------------------------------
+AppState::Code
+BulletPhysicsBasicApp::OnCleanup() {
+    this->discardPhysics();
+    Dbg::Discard();
+    this->discardGraphics();
+    return App::OnCleanup();
+}
+
+//------------------------------------------------------------------------------
+void
+BulletPhysicsBasicApp::setupGraphics() {
+
     auto gfxSetup = GfxSetup::WindowMSAA4(800, 600, "BulletPhysicsBasic");
     Gfx::Setup(gfxSetup);
-    Dbg::Setup();
 
     // setup graphics shapes
     ShapeBuilder shapeBuilder;
@@ -65,20 +106,6 @@ BulletPhysicsBasicApp::OnInit() {
         .Sphere(1, 13, 7)
         .Box(1.5f, 1.5f, 1.5f, 1);
     this->drawState.Mesh[0] = Gfx::CreateResource(shapeBuilder.Build());
-
-    // create textures
-    auto texSetup = TextureSetup::FromPixelData(4, 1, 1, TextureType::Texture2D, PixelFormat::RGBA8);
-    texSetup.ImageData.Sizes[0][0] = sizeof(BallImageData);
-    this->ballTexture = Gfx::CreateResource(texSetup, BallImageData, sizeof(BallImageData));
-
-    for (int i = 0, x = 0; x < GroundTexSize; x++, i++) {
-        for (int y = 0; y < GroundTexSize; y++, i++) {
-            GroundImageData[x][y] = i&1 ? 0xFF8080B0 : 0xFFB08080;
-        }
-    }
-    texSetup = TextureSetup::FromPixelData(GroundTexSize, GroundTexSize, 1, TextureType::Texture2D, PixelFormat::RGBA8);
-    texSetup.ImageData.Sizes[0][0] = sizeof(GroundImageData);
-    this->groundTexture = Gfx::CreateResource(texSetup, GroundImageData, sizeof(GroundImageData));
 
     // create pipeline state
     Id shd = Gfx::CreateResource(Shader::Setup());
@@ -92,24 +119,68 @@ BulletPhysicsBasicApp::OnInit() {
     const float fbHeight = (const float) Gfx::DisplayAttrs().FramebufferHeight;
     this->proj = glm::perspectiveFov(glm::radians(45.0f), fbWidth, fbHeight, 0.1f, 200.0f);
     this->view = glm::lookAt(glm::vec3(0.0f, 25.0f, -50.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-
-    // setup the physics world
-    Physics::Setup();
-    this->ground = Physics::Create(RigidBodySetup::Plane(glm::vec4(0, 1, 0, 0), 0.25f));
-    Physics::Add(this->ground);
-
-    this->lapTimePoint = Clock::Now();
-    return App::OnInit();
 }
 
 //------------------------------------------------------------------------------
-AppState::Code
-BulletPhysicsBasicApp::OnRunning() {
+void
+BulletPhysicsBasicApp::discardGraphics() {
+    Gfx::Discard();
+}
 
-    this->frameIndex++;
-    Duration frameTime = Clock::LapTime(this->lapTimePoint);
+//------------------------------------------------------------------------------
+void
+BulletPhysicsBasicApp::drawGround() {
+    Gfx::ApplyDrawState(this->drawState);
+    glm::mat4 groundTransform = Physics::Transform(this->ground);
+    this->vsParams.ModelViewProjection = this->proj * this->view * groundTransform;
+    Gfx::ApplyUniformBlock(this->vsParams);
+    Gfx::Draw(0);
+}
 
-    // emit new balls
+//------------------------------------------------------------------------------
+void
+BulletPhysicsBasicApp::drawRigidBodies() {
+    // FIXME: use instancing
+    Gfx::ApplyDrawState(this->drawState);
+    for (int i = 0; i < this->numBodies; i++) {
+        glm::mat4 ballTransform = Physics::Transform(this->bodies[i]);
+        this->vsParams.ModelViewProjection = this->proj * this->view * ballTransform;
+        Gfx::ApplyUniformBlock(this->vsParams);
+        if (Physics::ShapeType(this->bodies[i]) == RigidBodySetup::SphereShape) {
+            // a sphere
+            Gfx::Draw(1);
+        }
+        else {
+            // a box
+            Gfx::Draw(2);
+        }
+    }
+}
+
+//------------------------------------------------------------------------------
+void
+BulletPhysicsBasicApp::setupPhysics() {
+    Physics::Setup();
+    this->ground = Physics::Create(RigidBodySetup::Plane(glm::vec4(0, 1, 0, 0), 0.25f));
+    Physics::Add(this->ground);
+}
+
+//------------------------------------------------------------------------------
+void
+BulletPhysicsBasicApp::discardPhysics() {
+    Physics::Destroy(this->ground);
+    for (int i = 0; i < this->numBodies; i++) {
+        Physics::Destroy(this->bodies[i]);
+    }
+    Physics::Discard();
+}
+
+//------------------------------------------------------------------------------
+Duration
+BulletPhysicsBasicApp::updatePhysics(Duration frameTime) {
+    TimePoint physStartTime = Clock::Now();
+
+    // emit new rigid bodies
     if ((this->frameIndex % 10) == 0) {
         if (this->numBodies < MaxNumBodies) {
             Id newObj;
@@ -130,60 +201,7 @@ BulletPhysicsBasicApp::OnRunning() {
         }
     }
 
-    TimePoint physStartTime = Clock::Now();
+    // step the physics world
     Physics::Update(frameTime.AsSeconds());
-    Duration physTime = Clock::Since(physStartTime);
-    Gfx::ApplyDefaultRenderTarget(ClearState::ClearAll(glm::vec4(0.2f, 0.4f, 0.8f, 1.0f), 1.0f, 0));
-
-    // draw ground
-    this->drawState.FSTexture[0] = this->groundTexture;
-    Gfx::ApplyDrawState(this->drawState);
-    glm::mat4 groundTransform = Physics::Transform(this->ground);
-    this->vsParams.ModelViewProjection = this->proj * this->view * groundTransform;
-    this->vsParams.Color = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
-    Gfx::ApplyUniformBlock(this->vsParams);
-    Gfx::Draw(0);
-
-    // draw balls (fixme: use instancing)
-    this->drawState.FSTexture[0] = this->ballTexture;
-    Gfx::ApplyDrawState(this->drawState);
-    for (int i = 0; i < this->numBodies; i++) {
-        glm::mat4 ballTransform = Physics::Transform(this->bodies[i]);
-        this->vsParams.ModelViewProjection = this->proj * this->view * ballTransform;
-        this->vsParams.Color = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
-        Gfx::ApplyUniformBlock(this->vsParams);
-        if (Physics::ShapeType(this->bodies[i]) == RigidBodySetup::SphereShape) {
-            // a sphere
-            Gfx::Draw(1);
-        }
-        else {
-            // a box
-            Gfx::Draw(2);
-        }
-    }
-
-    Dbg::PrintF("\n\r"
-                "  Frame time:   %.4f ms\n\r"
-                "  Physics time: %.4f ms\n\r"
-                "  Num Bodies:   %d\n\r",
-                frameTime.AsMilliSeconds(),
-                physTime.AsMilliSeconds(),
-                this->numBodies);
-    Dbg::DrawTextBuffer();
-    Gfx::CommitFrame();
-    return Gfx::QuitRequested() ? AppState::Cleanup : AppState::Running;
-}
-
-//------------------------------------------------------------------------------
-AppState::Code
-BulletPhysicsBasicApp::OnCleanup() {
-
-    Physics::Destroy(this->ground);
-    for (int i = 0; i < this->numBodies; i++) {
-        Physics::Destroy(this->bodies[i]);
-    }
-    Physics::Discard();
-    Dbg::Discard();
-    Gfx::Discard();
-    return App::OnCleanup();
+    return Clock::Since(physStartTime);
 }
