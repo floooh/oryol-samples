ce4bf33bb9d231586e12f7e74ab1f6c3c4f7b4b2
Andre Weissflog <floooh@gmail.com>
2018-04-24 18:38:18 +0200

    Remove the obsolete ImGuiAdvanced demo

--- src/ImGuiAdvanced/font.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-// #version:4#
-// machine generated, do not edit!
-namespace Oryol {
-extern unsigned char dump_roboto_regular[515100];
-extern unsigned char dump_font[28464];
-} // namespace Oryol
